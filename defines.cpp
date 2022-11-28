7536f55e6a6b995277faf67eefa5d45807ef9aae
Mikhaylov Gleb <geba_m@mail.ru>
2022-11-28 23:35:07 +0700

    Create defines.cpp

---
+++ defines.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+
+#define pb push_back
+#define pf push_front
+#define F first
+#define INF 10e9 + 7
+#define S second
+#define endl "\n"
+#define cin(x) for(auto &o : x) cin >> o
+#define cout(x) for(auto o : x) cout << o << ' '; cout << '\n'
+#define loop(i, x, n) for(ll i = i; i < n; i++)
+#define eps 1e-12
+#define sqr(a) ((a)*(a))
+#define fast ios_base::sync_with_stdio(0), cin.tie(0), cout.tie(0);
+#define END {cout<< 0; return 0}
+
+using namespace std;
+typedef long long ll;
+typedef long double ld;
+
+
+int main() {
+    fast;
+    
+    return 0;
+}
